c467ed12effdb45b02b223a85f5343505a2c66d2
vsukiasyan <vsukiasyan1@hotmail.com>
2018-05-07 14:57:34 -0700

    Added CoreML - SqueezeNet for object identification. Tested a few household objects, seems to work accurately for the most part.

--- Optics/CameraVC.swift
+++ Optics/CameraVC.swift
@@ -83,6 +83,23 @@ class CameraVC: UIViewController {
         cameraOutput.capturePhoto(with: settings, delegate: self)
     }
     
+    func resultsMethod(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else { return }
+        
+        for classification in results {
+            print(classification.identifier)
+            if classification.confidence < 0.5 {
+                self.identificationLbl.text = "I'm not sure what this is. Please try again"
+                self.confidenceLbl.text = ""
+                break
+            } else {
+                self.identificationLbl.text = classification.identifier
+                self.confidenceLbl.text = "CONFIDENCE: \(Int(classification.confidence * 100))%"
+                break
+            }
+        }
+    }
+    
 
 }
 
@@ -92,6 +109,15 @@ extension CameraVC: AVCapturePhotoCaptureDelegate {
             debugPrint(error)
         } else {
             photoData = photo.fileDataRepresentation()
+            
+            do {
+                let model = try VNCoreMLModel(for: SqueezeNet().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: resultsMethod)
+                let handler = VNImageRequestHandler(data: photoData!)
+                try handler.perform([request])
+            } catch {
+                debugPrint(error)
+            }
             
             let image = UIImage(data: photoData!)
             self.captureImageView.image = image
